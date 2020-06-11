ae3c7ee9f80ff3df59e500e11a8e470581929236
Matt Roberts <justskiit@gmail.com>
2020-06-11 16:54:03 -0600

    Saving progress

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+// connect to db
 mongoose.connect('mongodb://localhost/two-cows', {useNewUrlParser: true,  useUnifiedTopology: true});
 
-var CowSchema = new Schema({
-  name: String;
-  description: String;
+const CowSchema = new Schema({
+  name: String,
+  description: String
 });
+//Every model has an associated connection. When you use mongoose.model(), your model will use the default mongoose connection.
+const Cow = mongoose.model('Cow', CowSchema);
 
-module.exports = mongoose.model('Cow', CowSchema)
+const getAllCows = () => {
+  return Cow.find()
+}
+const addCow = (cowObj) => {
+  // create instance of cow to save a new one
+  console.log(cowObj);
+  let newCow = new Cow(cowObj);
+  newCow.save(function(err){
+    if(err) return handleError(err)
+  })
+}
+
+
+
+// export functions for use on server
+module.exports = {
+  getAllCows,
+  addCow
+}
--- server/index.js
+++ server/index.js
@@ -3,9 +3,38 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 8080;
+const bp = require('body-parser');
+// const mongoose = require('mongoose');
 // import Cow model
-const Cow = require('./db/db');
+const db = require('./db/db');
+
+// use body parser
+app.use(bp.json());
+app.use(bp.urlencoded({extended: false}))
+
+app.get('/api/cows', function(req, res){
+  // res.send('Get path works');
+  db.getAllCows()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send('Error - Cannot get cows');
+    })
+})
 
 
+app.post('/api/cows', (req, res) => {
+  // res.send('Post works!');
+  console.log(req.body); // left off here ++++++++++++++++++++++++++++++++++++++++++++
+  db.addCow(req.body)
+    .then( (data) => {
+      console.log('Post data= ', data);
+      res.status(201).send('New cow created!');
+    })
+    .catch( (err) => {
+      res.status(500).send('Error posting data')
+    })
+})
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Cow Server is running on port ${port}`));
