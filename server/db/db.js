598e8461cccd4f652c235fba761bc7c40bc0269e
Matt Roberts <justskiit@gmail.com>
2020-06-11 14:06:17 -0600

    db and server setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/two-cows', {useNewUrlParser: true,  useUnifiedTopology: true});
+
+var CowSchema = new Schema({
+  name: String;
+  description: String;
+});
+
+module.exports = mongoose.model('Cow', CowSchema)
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+// web server setup
 const express = require('express');
-const cors = require('cors')
-
 const app = express();
+const cors = require('cors');
 const port = 8080;
+// import Cow model
+const Cow = require('./db/db');
+
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
